c39823bc2cf60a5a098a0d773d74a9afb212d894
Ashrqat19 <71236765+Ashrqat19@users.noreply.github.com>
2021-09-03 02:16:44 +0200

    getStartedAlready

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+$("body").niceScroll({
+    cursorcolor: "black",
+    cursorwidth: "8px",
+    // cursorborderradius: 0,
+    cursorborder: "1px solid black",
+});
+
+$(function() {
+    'use strict';
+    $('.header').height($(window).height());
+});
